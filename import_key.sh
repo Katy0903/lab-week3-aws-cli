57ceac0198151d1c3579085757c3688275d1b15e
Katy_wsl2_ubuntu <cchan532@my.bcit.ca>
2025-01-26 17:48:02 -0800

    updated create-ec2 file

--- import_key.sh
+++ import_key.sh
@@ -5,6 +5,7 @@ KEY_PATH="~/.ssh/bcitkey.pub"
 REGION="us-west-2"
 
 
-aws ec2 import-key-pair --key-name "$KEY_NAME" --public-key-material file://$KEY_PATH --region "$REGION"
-
-#filed or file??
+aws ec2 import-key-pair \
+    --key-name "$KEY_NAME" \
+    --public-key-material fileb://$KEY_PATH \
+    --region "$REGION"
